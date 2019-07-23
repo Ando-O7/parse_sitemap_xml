f293c92a79e3b673410e77605ece9ff6d3ac0382
ando07 <ando.masahito@gmail.com>
2019-07-24 08:21:10 +0900

    [modify]Functionalization of read sitemap and error check

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -16,24 +18,27 @@ type Sitemap struct {
 }
 
 func main() {
-	// Read xml file
 	url := "https://www.google.com/sitemap.xml"
+	sitemap := GetSitemap(url)
+	for _, page := range sitemap {
+		fmt.Println(page.Loc)
+	}
+}
+
+func GetSitemap(url string) []Page {
 	response, err := http.Get(url)
+	checkErr(err)
 
-	// check error
+	XMLdata, err := ioutil.ReadAll(response.Body)
+	checkErr(err)
+
+	var sitemap Sitemap
+	xml.Unmarshal(XMLdata, &sitemap)
+	return sitemap.Pages
+}
+
+func checkErr(err error) {
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	fmt.Println("status:", response.Status)
-
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(body))
 }
